4a0f85889d19adeb582a2749b8d14b3010f08dee
joemyson <joemyson1402@gmail.com>
2026-02-24 16:32:52 -0300

    project created

---
+++ src/test/java/com/joemyson/sistema_de_cadastro/SistemaDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joemyson.sistema_de_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
